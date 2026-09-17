394f2411cf4d09d4cb0f01be7143a5706524655a
FrederikSkipper <RK23KN@student.aau.dk>
2026-09-17 23:17:47 +0200

    feat: added packages for db, connectionString to settings and began builder for DB-context.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.EntityFrameWorkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddDbContext<CoreDbContext>(options => options.useNpgsql(builder.Configuration.getConnectionString("ConnPostgreSQL")));
 
 var app = builder.Build();
 
